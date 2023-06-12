ad7f86b4c8caefdcab05d989955ca21d8b83bf96
Andrea Mussari <a.mussari@studenti.unipi.it>
2023-06-13 00:26:39 +0200

    Update generale

--- archivio.c
+++ archivio.c
@@ -16,50 +16,17 @@
 #define PC_buffer_len 10
 #define Max_sequence_length 2048
 
-static int lenhash = 0;
-static ENTRY *testa = NULL;
-
 typedef struct
 {
-    int *nthread;
-    char *fifo;
     char **buffer;
-    int *index;
-    pthread_mutex_t *mutex;
-    pthread_cond_t *full;
-    pthread_cond_t *empty;
-    bool *continua;
-    pthread_mutex_t *hashmutex;
-    pthread_cond_t *hash;
-} argCapi;
+    int counter;
+    pthread_mutex_t *hmutex;// hash table
+    pthread_mutex_t *bfmutex; // buffer PC
+    pthread_cond_t hcond; // hash table
+    pthread_cond_t empty; // buffer PC
+    pthread_cond_t full; // buffer PC
+}SLarg;
 
-typedef struct
-{
-    int *index;
-    char **buffer;
-    pthread_mutex_t *mutex;
-    pthread_cond_t *full;
-    pthread_cond_t *empty;
-    pthread_mutex_t *hashmutex;
-    pthread_cond_t *hash;
-} argT;
-
-typedef struct
-{
-    int *lenhash;
-    pthread_t *CS;
-    pthread_t *CL;
-    bool *continua;
-    pthread_mutex_t *mutex;
-    pthread_cond_t *hash;
-
-} argSig;
-
-typedef struct
-{
-    int valore; // numero di occorrenze della stringa
-    ENTRY *next;
-} coppia;
 
 void termina(const char *messaggio)
 {
@@ -82,10 +49,7 @@ ENTRY *crea_entry(char *s)
     if (e->key == NULL || e->data == NULL)
     {
         termina("errore malloc entry 2");
-    }
-    coppia *c = (coppia *)e->data; // cast obbligatorio
-    c->valore = 1;
-    c->next = NULL;
+    *((int *)e->data) = 1;
     return e;
 }
 
@@ -108,15 +72,12 @@ void aggiungi(char *s)
         {
             termina("errore o tabella piena");
         }
-        coppia *c = (coppia *)e->data;
-        c->next = testa;
-        testa = e;
     }
     else
     {
         assert(strcmp(e->key, r->key) == 0);
-        coppia *c = (coppia *)r->data;
-        c->valore += 1;
+        int *TMP = (int *)r->data;
+        *TMP +=1;
         distruggi_entry(e);
     }
 }
@@ -125,291 +86,30 @@ int conta(char *s)
 {
     ENTRY *e = crea_entry(s);
     ENTRY *r = hsearch(*e, FIND);
-    if (r == NULL)
+    int n = 0;
+    if (r != NULL)
     {
-        return 0;
-    }
-    else
-    {
-        return *((int *)r->data);
+        assert(strcmp(e->key, r->key) == 0);
+        n = *((int *)r->data);
     }
     distruggi_entry(e);
+    return n;
 }
 
-void *Scrittori(void *arg)
-{
-    printf("sono uno scrittore\n");
-    argT *a = (argT *)arg;
+void *Scrittori(void *args){
 
-    while (*(a->index) > 0)
-    {
-        pthread_mutex_lock(a->mutex);
-        while (*(a->index) == 0)
-        {
-            pthread_cond_wait(a->full, a->mutex);
-        }
-
-        char *s = a->buffer[*(a->index)-- % PC_buffer_len];
-        pthread_mutex_lock(a->hashmutex);
-        aggiungi(s);
-        pthread_cond_signal(a->hash);
-        pthread_mutex_unlock(a->hashmutex);
-        free(s);
-        pthread_cond_signal(a->empty);
-        pthread_mutex_unlock(a->mutex);
-    }
-
-    return NULL;
 }
 
-void *Lettori(void *arg)
-{
-    printf("sono un lettore\n");
-    argT *a = (argT *)arg;
-    FILE *f = fopen("lettori.log", "a");
-    if (f == NULL)
-    {
-        termina("errore apertura lettore.log");
-    }
+void *Lettori(void *args){
 
-    while (*(a->index) > 0)
-    {
-        pthread_mutex_lock(a->mutex);
-        while (*(a->index) == 0)
-        {
-            pthread_cond_wait(a->full, a->mutex);
-        }
-
-        char *s = a->buffer[*(a->index)-- % PC_buffer_len];
-        pthread_mutex_lock(a->hashmutex);
-        int n = conta(s);
-        pthread_cond_signal(a->hash);
-        pthread_mutex_unlock(a->hashmutex);
-        fprintf(f, "%s %d\n", s, n);
-        free(s);
-        pthread_cond_signal(a->empty);
-        pthread_mutex_unlock(a->mutex);
-    }
-    return NULL;
 }
 
-void *capoScrittori(void *arg)
-{
-    printf("sono il capo scrittori\n");
-    argCapi *a = (argCapi *)arg;
-    int fd = open(a->fifo, O_RDONLY);
-    if (fd == -1)
-    {
-        termina("errore apertura fifo");
-    }
-    int len, index = 0;
-    char buffer[Max_sequence_length];
-    char *saveptr;
+void *Capi(void *args){
 
-    pthread_t t[*(a->nthread)];
-    argT b[*(a->nthread)];
-    printf("creo i scrittori\n");
-    for (int i = 0; i < *(a->nthread); i++)
-    {
-        b[i].index = &index;
-        b[i].buffer = a->buffer;
-        b[i].mutex = a->mutex;
-        b[i].full = a->full;
-        b[i].empty = a->empty;
-        b[i].hashmutex = a->hashmutex;
-        b[i].hash = a->hash;
-        pthread_create(&t[i], NULL, &Scrittori, &b[i]);
-    }
-
-
-    if (read(fd, &len, sizeof(int)) != sizeof(int))
-    {
-        termina("errore read");
-    }
-
-    if (read(fd, buffer, len) != len)
-    {
-        termina("errore read");
-    }
-
-    buffer[len] = '\0';
-
-    char *token = strtok_r(buffer, ".,:; \n\r\t", &saveptr);
-
-    while (token != NULL)
-    {
-        char *s = strdup(token);
-        pthread_mutex_lock(a->mutex);
-        while (*(a->index) - 1 == PC_buffer_len)
-        {
-            pthread_cond_wait(a->empty, a->mutex);
-        }
-        a->buffer[*(a->index)++ % PC_buffer_len] = s;
-
-        pthread_cond_signal(a->full);
-        pthread_mutex_unlock(a->mutex);
-
-        pthread_mutex_lock(a->hashmutex);
-
-        if (a->continua == false)
-        {
-            for (int i = 0; i < *(a->nthread); i++)
-            {
-                pthread_join(t[i], NULL);
-            }
-            pthread_cond_signal(a->hash);
-            pthread_mutex_unlock(a->hashmutex);
-            break;
-        }
-
-        token = strtok_r(NULL, ".,:; \n\r\t", &saveptr);
-    }
-
-    close(fd);
-    return NULL;
 }
 
-void *capoLettori(void *arg)
-{
-    printf("sono il capo lettori\n");
-    argCapi *a = (argCapi *)arg;
-    int fd = open(a->fifo, O_RDONLY);
-    if (fd == -1)
-    {
-        termina("errore apertura fifo");
-    }
-    int len, index = 0;
-    char buffer[Max_sequence_length];
-    char *saveptr;
+void *Signal_handler(void *args){
 
-    pthread_t t[*(a->nthread)];
-    argT b[*(a->nthread)];
-
-    printf("creo i lettori\n");
-    for (int i = 0; i < *(a->nthread); i++)
-    {
-        b[i].index = &index;
-        b[i].buffer = a->buffer;
-        b[i].mutex = a->mutex;
-        b[i].full = a->full;
-        b[i].empty = a->empty;
-        b[i].hashmutex = a->hashmutex;
-        b[i].hash = a->hash;
-        pthread_create(&t[i], NULL, &Lettori, &b[i]);
-    }
-
-    if (read(fd, &len, sizeof(int)) != sizeof(int))
-    {
-        termina("errore read");
-    }
-
-    if (read(fd, buffer, len) != len)
-    {
-        termina("errore read");
-    }
-
-    buffer[len] = '\0';
-
-    char *token = strtok_r(buffer, ".,:; \n\r\t", &saveptr);
-
-    while (token != NULL)
-    {
-        char *s = strdup(token);
-        pthread_mutex_lock(a->mutex);
-        while (*(a->index) - 1 == PC_buffer_len)
-        {
-            pthread_cond_wait(a->empty, a->mutex);
-        }
-        a->buffer[*(a->index)++ % PC_buffer_len] = s;
-
-        pthread_cond_signal(a->full);
-        pthread_mutex_unlock(a->mutex);
-
-        pthread_mutex_lock(a->hashmutex);
-
-        if (a->continua == false)
-        {
-            for (int i = 0; i < *(a->nthread); i++)
-            {
-                pthread_join(t[i], NULL);
-            }
-            pthread_cond_signal(a->hash);
-            pthread_mutex_unlock(a->hashmutex);
-            break;
-        }
-
-        token = strtok_r(NULL, ".,:; \n\r\t", &saveptr);
-    }
-
-    close(fd);
-    return NULL;
-}
-
-void *sigHandler(void *arg)
-{
-    printf("sono il sigHandler\n");
-    argSig *a = (argSig *)arg;
-    sigset_t set;
-    sigemptyset(&set);
-    sigaddset(&set, SIGINT);
-    sigaddset(&set, SIGTERM);
-    sigaddset(&set, SIGUSR1);
-    pthread_sigmask(SIG_BLOCK, &set, NULL);
-
-    int s;
-
-    while (*(a->continua))
-    {
-        int e = sigwait(&set, &s);
-        if (e != 0)
-        {
-            termina("errore sigwait");
-        }
-        if (s == SIGINT)
-        {
-            fprintf(stderr, "# stringhe distinte nella tabella hash :%d\n", *(a->lenhash));
-        }
-        if (s == SIGTERM)
-        {
-            printf("sono in sigterm\n");
-            pthread_join(*(a->CS), NULL);
-            pthread_join(*(a->CL), NULL);
-            printf("# stringhe distinte nella tabella hash :%d\n", *(a->lenhash));
-
-            pthread_mutex_lock(a->mutex);
-            ENTRY *current = testa;
-            ENTRY *next;
-
-            while (current != NULL)
-            {
-                next = ((coppia*)current->data)->next;
-                distruggi_entry(current);
-                current = next;
-            }
-            hdestroy();
-            pthread_cond_signal(a->hash);
-            pthread_mutex_unlock(a->mutex);
-            return NULL;
-        }
-        else if (s == SIGUSR1)
-        {
-            pthread_mutex_lock(a->mutex);
-            ENTRY *current = testa;
-            ENTRY *next;
-
-            while (current != NULL)
-            {
-                next = ((coppia*)current->data)->next;
-                distruggi_entry(current);
-                current = next;
-            }
-            hdestroy();
-            hcreate(Num_elem);
-            pthread_cond_signal(a->hash);
-            pthread_mutex_unlock(a->mutex);
-        }
-    }
-    return NULL;
 }
 
 int main(int argc, char *argv[])
@@ -418,66 +118,11 @@ int main(int argc, char *argv[])
     {
         termina("Uso: ./archivio <w> <r>\n");
     }
-    int hashtable = hcreate(Num_elem);
-    if (hashtable == 0)
-    {
-        termina("Errore hcreate\n");
-    }
-    int w = atoi(argv[1]);
-    int r = atoi(argv[2]);
-    int indexS = 0;
-    int indexL = 0;
 
-    pthread_mutex_t mutexS = PTHREAD_MUTEX_INITIALIZER;
-    pthread_mutex_t mutexL = PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t fullS = PTHREAD_COND_INITIALIZER;
-    pthread_cond_t emptyS = PTHREAD_COND_INITIALIZER;
-    pthread_cond_t fullL = PTHREAD_COND_INITIALIZER;
-    pthread_cond_t emptyL = PTHREAD_COND_INITIALIZER;
-    char *bufferS[PC_buffer_len];
-    char *bufferL[PC_buffer_len];
+    pthread_t writers[scrittori];
+    pthread_t readers[lettori];
+    pthread_t capi[2]; // 0: lettori, 1: scrittori
 
-    pthread_mutex_t mutexH = PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t hash = PTHREAD_COND_INITIALIZER;
-    bool continua = false;
-
-    argSig arg;
-    argCapi args[2]; // 0 -> scrittori, 1 -> lettori
-    pthread_t capi[2];
-    pthread_t sigH;
-
-    args[0].nthread = &w;
-    args[0].fifo = "caposc";
-    args[0].buffer = bufferS;
-    args[0].index = &indexS;
-    args[0].mutex = &mutexS;
-    args[0].full = &fullS;
-    args[0].empty = &emptyS;
-    args[0].hash = &hash;
-    args[0].hashmutex = &mutexH;
-
-    args[1].nthread = &r;
-    args[1].fifo = "capolet";
-    args[1].buffer = bufferL;
-    args[1].index = &indexL;
-    args[1].mutex = &mutexL;
-    args[1].full = &fullL;
-    args[1].empty = &emptyL;
-    args[1].hash = &hash;
-    args[1].hashmutex = &mutexH;
-
-    arg.lenhash = &lenhash;
-    arg.mutex = &mutexH;
-    arg.hash = &hash;
-    arg.continua = &continua;
-    arg.CS = &capi[0];
-    arg.CL = &capi[1];
-
-    pthread_create(&capi[0], NULL, capoScrittori, &args[0]);
-    pthread_create(&capi[1], NULL, capoLettori, &args[1]);
-    pthread_create(&sigH, NULL, sigHandler, &arg);
-
-    pthread_join(sigH, NULL);
-    
     return 0;
 }
+
--- server.py
+++ server.py
@@ -1,94 +1,41 @@
 #! /usr/bin/env python3
 
-import struct, socket,logging,os,argparse,subprocess,signal,concurrent.futures,threading,sys
+import socket
 
-def main(host,port,nthreads):
-     # creiamo il server socket
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:  
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)            
-            s.bind((host, port))
-            s.listen()
-            with concurrent.futures.ThreadPoolExecutor(max_workers=nthreads) as executor:
-                while True:
-                    print("In attesa di un client...")
-                    # mi metto in attesa di una connessione
-                    conn, addr = s.accept()
-                    # l'esecuzione di submit non è bloccante
-                    # fino a quando ci sono thread liberi
-                    executor.submit(gestisci_connessione, conn,addr)
-        except KeyboardInterrupt:
-            print("Chiusura server in corso...")
-            s.shutdown(socket.SHUT_RDWR)
-            os.unlink("capolet")
-            os.unlink("caposc")
-            p.send_signal(signal.SIGTERM)
+HOST = "127.0.0.1"
+PORT = 57943 #637943
 
-def gestisci_connessione(conn,addr): 
-  # L'uso di with serve solo a garantire che
-  # conn venga chiusa all'uscita del blocco
-  print(f"Contattato da {addr}")
-  with conn:
-    typecon = conn.recv(1).decode('utf-8')
-    if typecon == "0":
-        data = conn.recv(2048)
-        linea = data.decode('utf-8')
-        logging.debug(f"TIPO A Scritto: {len(data)} bytes")
-        #lock.acquire()
-        fifo = os.open("capolet", os.O_WRONLY)
-        os.write(fifo, linea.encode('utf-8'))
-        os.close(fifo)
-        #lock.release()
-    elif typecon == "1":
-        total_sequences = 0
-        totb = 0
-        while True:
-            data = conn.recv(2048)
-            if data.decode('utf-8') == "STOP":
-                conn.sendall(total_sequences.to_bytes(4, byteorder='big'))
-                break
-            #lock.acquire()
-            fifo = os.open("caposc", os.O_WRONLY)
-            os.write(fifo, data.encode('utf-8'))
-            os.close(fifo)
-            #lock.release()
-            total_sequences += 1
-            totb += len(data)
-        logging.debug(f"TIPO B Scritto: {totb} bytes")
-        print("diomerda")
+# creazione del server socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()   
+    while True:
+      print(f"In attesa di un client su porta {PORT}...")
+      # mi metto in attesa di una connessione
+      conn, addr = s.accept()
+      # lavoro con la connessione appena ricevuta 
+      with conn:  
+         print(f"Contattato da {addr}")
+         tipo = conn.recv(1)
+         if tipo.decode() == "0":
+            print("Connessione A")
+            parola = conn.recv(2048).decode().strip("\x00").rstrip('\n')
+            print(f"Parola ricevuta:{parola}")
+            with open("capolet","wb") as f:
+               f.write(parola.encode())
+            print("chiusa connessione")
 
-if __name__ == '__main__':
-
-    if(os.path.exists("./capolet")) == False:
-        os.mkfifo("capolet")
-    
-    if(os.path.exists("./caposc")) == False:
-        os.mkfifo("caposc")
-    
-    if(os.path.exists("./server.log")) == False:
-        open("server.log", "x")
-
-    logging.basicConfig(filename='./server.log',level=logging.DEBUG,filemode='w',datefmt='%d/%m/%y %H:%M:%S', format='%(asctime)s - %(levelname)s - %(message)s')
-    Description = "Server"
-
-    HOST = "127.0.0.1"
-    PORT = 57943 # MAT:637943
-
-    lock = threading.Lock()
-
-    parser = argparse.ArgumentParser(description=Description, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("-r",type=int,help="numero thread lettori",default=3)
-    parser.add_argument("-w",type=int,help="numero thread scrittori",default=3)
-    parser.add_argument("-v",help="chiamare il programma archivio mediante valgrind con opzioni valgrind --leak-check=full --show-leak-kinds=all --log-file=valgrind-%p.log",action='store_true')
-    parser.add_argument("nthread",type=int,help="numero massimo di thread che il server deve utilizzare contemporanemente per la gestione dei client")
-    args = parser.parse_args()
-    if args.v:
-        p = subprocess.Popen(["valgrind","--leak-check=full", 
-        "--show-leak-kinds=all", 
-                    "--log-file=valgrind-%p.log", 
-                    "./archivio", f"{args.w}", f"{args.r}"])
-    else:
-        p = subprocess.Popen(["./archivio", f"{args.w}", f"{args.r}"])
-
-    main(nthreads=args.nthread,host=HOST,port=PORT)
-    
+         elif tipo.decode() == "1":
+            totseq = 0
+            print("connessione B")
+            while True:
+               parola = conn.recv(2048).decode().strip("\x00").rstrip('\n')
+               print(f"Parola ricevuta:{parola}")
+               if parola == "":
+                  print(totseq)
+                  conn.sendall(totseq.to_bytes(4, byteorder='big'))
+                  break
+               totseq += 1
+               with open("caposc","wb") as f:
+                  f.write(parola.encode())
+            print("chiusa connessione")
