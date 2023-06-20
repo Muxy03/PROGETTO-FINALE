c48ba9f1889dd6b7f7048a86bb5fccf3c638f9af
Andrea Mussari <99414759+Muxy03@users.noreply.github.com>
2023-06-20 13:51:42 +0200

    Update client1

--- client1.c
+++ client1.c
@@ -9,7 +9,6 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-// host e port a cui connettersi
 #define HOST "127.0.0.1"
 #define PORT 57943 // 637943
 #define Max_sequence_length 2048
@@ -18,10 +17,13 @@
 void termina(const char *messaggio)
 {
     if (errno == 0)
+    {
         fprintf(stderr, "== %d == %s\n", getpid(), messaggio);
+    }
     else
-        fprintf(stderr, "== %d == %s: %s\n", getpid(), messaggio,
-                strerror(errno));
+    {
+        fprintf(stderr, "== %d == %s: %s\n", getpid(), messaggio, strerror(errno));
+    }
     exit(1);
 }
 
@@ -32,7 +34,7 @@ int main(int argc, char const *argv[])
         termina("Uso ./client1 nomefile");
     }
 
-    int fd = 0; // file descriptor associato al socket
+    int fd = 0;
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
@@ -40,6 +42,7 @@ int main(int argc, char const *argv[])
 
     char *line = NULL;
     size_t len = 0;
+    ssize_t size = 0;
 
     FILE *f = fopen(argv[1], "r");
 
@@ -48,12 +51,15 @@ int main(int argc, char const *argv[])
         if (strlen(line) > 0 && strlen(line) <= Max_sequence_length)
         {
             if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+            {
                 termina("Errore creazione socket");
+            }
 
             if (connect(fd, &serv_addr, sizeof(serv_addr)) < 0)
+            {
                 termina("Errore apertura connessione");
+            }
 
-            ssize_t size;
             size = write(fd, tipoc, sizeof(tipoc));
 
             size = send(fd, line, strlen(line), 0);
