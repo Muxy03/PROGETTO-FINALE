c86dfb3984f5f4520f3a2ce399453fb4a5fafd20
Andrea Mussari <99414759+Muxy03@users.noreply.github.com>
2023-07-02 10:55:23 +0200

    Update server.py FINALE

--- server.py
+++ server.py
@@ -5,24 +5,27 @@ import socket, concurrent.futures, argparse, os, subprocess, logging, signal
 HOST = "127.0.0.1"
 PORT = 57943 #637943
 
-def main(max_threads):
+def main(max_threads):    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind((HOST, PORT))
-            s.listen()
-            with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
-                while True:
-                    conn, addr = s.accept()
-                    executor.submit(gestisci_connessione, conn, addr)
-        except KeyboardInterrupt:
-            s.shutdown(socket.SHUT_RDWR)
-            p.send_signal(signal.SIGTERM)
-            p.wait()
-            os.unlink("capolet")
-            os.unlink("caposc")
+            try:
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                s.bind((HOST, PORT))
+                s.listen()
+                with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+                    while True:
+                        conn, _ = s.accept()
+                        executor.submit(gestisci_connessione, conn)
+            except KeyboardInterrupt:
+                s.shutdown(socket.SHUT_RDWR)
+                print("Chiusura server in corso...")
+                p.send_signal(signal.SIGTERM)
+                p.wait()
+                os.unlink("capolet")
+                os.unlink("caposc")
+                print("Server chiuso")
 
-def gestisci_connessione(conn, addr):
+def gestisci_connessione(conn):
+
     tipo = conn.recv(1).decode()
     totaleb = 0
 
@@ -30,29 +33,32 @@ def gestisci_connessione(conn, addr):
         parola = conn.recv(2048).decode().strip("\x00").rstrip('\n')
         with open("capolet", "wb") as fifo:
             lunghezza=len(parola)
-            st="0"*(4-lunghezza) + str(lunghezza)
-            fifo.write(st.encode()+parola.encode())
+            st="0"*(4-len(str(lunghezza))) + str(lunghezza)
+            fifo.write(st.encode())
+            fifo.write(parola.encode())
             totaleb += len(parola.encode())
 
     elif tipo == "1":
+        
         totales = 0
-
         parola = conn.recv(2048)
-        lunghezza=len(parola.decode().strip("\x00").rstrip('\n'))
+        lunghezza=len(parola.decode().strip('\x00').rstrip('\n'))
+        
         with open("caposc", "wb") as fifo:
-            while parola.decode() != "":
+            while parola.decode() != "STOP":
+                st="0"*(4-len(str(lunghezza))) + str(lunghezza)
+                fifo.write(st.encode())
+                fifo.write(parola.decode().strip('\x00').rstrip('\n').encode())
                 totales+=1
-                st="0"*(4-lunghezza) + str(lunghezza)
-                fifo.write(st.encode()+parola)
                 totaleb += len(parola)
                 parola = conn.recv(2048)
-                lunghezza=len(parola.decode().strip("\x00").rstrip('\n'))
-        
+                lunghezza=len(parola.decode().strip('\x00').rstrip('\n'))
+                
         conn.sendall(totales.to_bytes(4,"big"))
-        print("chiusura connessione")
+        
+    print("chiudo connessione")
 
-    logging.debug("Client con connessione di tipo %s ha scritto %d bytes", tipo.decode(), totaleb)
-
+    logging.debug("Client con connessione di tipo %s ha scritto %d bytes", tipo, totaleb)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Server", formatter_class=argparse.RawTextHelpFormatter)
@@ -70,9 +76,15 @@ if __name__ == "__main__":
     if not os.path.exists("capolet"):
         os.mkfifo("capolet")
 
+    with open("lettori.log","w") as f:
+        pass
+
     if args.v :
         p = subprocess.Popen(["valgrind","--leak-check=full","--show-leak-kinds=all","--log-file=valgrind-%p.log","./archivio",f"{args.w}",f"{args.r}"])
     else :
         p = subprocess.Popen(["./archivio",f"{args.w}",f"{args.r}"])
-
+    
     main(args.t)
+    
+   
+
